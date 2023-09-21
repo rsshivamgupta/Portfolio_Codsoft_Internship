a8cc719545e908a6bdfccf7f40826b21cad50910
Shivam Kumar Gupta <67995955+shivamgupta7369@users.noreply.github.com>
2023-09-21 19:22:45 +0530

    Add files via upload

---
+++ portfolio/main.js
@@ -0,0 +1,167 @@
+<section>
+<div class="services" id="services">
+                <div class="container">
+                    <h1 class="sub-title"> my <span>services</span></h1>
+                    <div class="services-list">
+                        <div>
+                            <i class='bx bx-code-alt'
+                                style="font-size: 30px; color: aqua;"></i>
+                            <h2>UI/UX DESIGN</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Ratione quis accusantium
+                                at itaque neque rem sunt aliquid, consequuntur
+                                in tenetur fugiat eaque sapiente.
+                                Ducimus id suscipit error repellat laboriosam
+                                perspiciatis.</p>
+                            <a href="#" class="read">LEARN MORE </a>
+                        </div>
+
+                        <div>
+                            <i class='bx bx-crop'
+                                style="font-size: 30px; color: aqua;"></i>
+                            <h2>UI/UX DESIGN</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Ratione quis accusantium
+                                at itaque neque rem sunt aliquid, consequuntur
+                                in tenetur fugiat eaque sapiente.
+                                Ducimus id suscipit error repellat laboriosam
+                                perspiciatis.</p>
+                            <a href="#" class="read">LEARN MORE </a>
+                        </div>
+
+                        <div>
+                            <i class='bx bxl-apple'
+                                style="font-size: 30px; color: aqua;"></i>
+                            <h2>UI/UX DESIGN</h2>
+                            <p>Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Ratione quis accusantium
+                                at itaque neque rem sunt aliquid, consequuntur
+                                in tenetur fugiat eaque sapiente.
+                                Ducimus id suscipit error repellat laboriosam
+                                perspiciatis.</p>
+                            <a href="#" class="read">LEARN MORE </a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <h1 class="sub-title">MY <span>SKILL</span></h1>
+
+        <section>
+            <div class="container1" id="Skills">
+                <h1 class="heading1">Technical Skills</h1>
+                <div class="technical-bars">
+                    <div class="bar"> <i style="color: rgb(255, 0, 0);" class='bx bxl-html5'></i>
+                        <div class="info">
+                            <span>HTML</span>
+                        </div>
+                        <div class="progress-line html">
+                            <span></span>
+
+                        </div>
+
+                    </div>
+
+                    <div class="bar"> <i style="color: rgb(17, 0, 255);" class='bx bxl-css3'></i>
+                        <div class="info">
+                            <span>CSS</span>
+                        </div>
+                        <div class="progress-line css">
+                            <span></span>
+
+                        </div>
+
+                    </div>
+
+                    <div class="bar"> <i style="color: rgb(251, 255, 0);" class='bx bxl-javascript'></i>
+                        <div class="info">
+                            <span>JAVASCRIPT</span>
+                        </div>
+                        <div class="progress-line javascript">
+                            <span></span>
+
+                        </div>
+
+                    </div>
+
+                    <div class="bar"> <i style="color: rgb(17, 0, 255);"  class='bx bxl-jquery'></i>
+                        <div class="info">
+                            <span>JQUERY</span>
+                        </div>
+                        <div class="progress-line jquery">
+                            <span></span>
+
+                        </div>
+
+                    </div>
+
+                    <div class="bar"> <i style="color: rgb(247, 0, 255);"  class='bx bxl-bootstrap'></i>
+                        <div class="info">
+                            <span>BOOTSTRAP</span>
+                        </div>
+                        <div class="progress-line bootstrap">
+                            <span></span>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="container1">
+                <h1 class="heading1">Professional Skill</h1>
+                <div class="radial-bars">
+
+                    <svg x="0px" y="0px" viewBox="0 0 200 200">
+                        <circle class="progress-bar" cx="100" cy="100" r="80"></circle>
+                        <circle class="path path-1" cx="100" cy="100" r="80"></circle>
+                    </svg>
+
+                    <div class="percentage">90%</div>
+                    <div class="text">Creativity</div>
+
+                </div>
+
+                <div class="radial-bars">
+
+                    <svg x="0px" y="0px" viewBox="0 0 200 200">
+                        <circle class="progress-bar" cx="100" cy="100" r="80"></circle>
+                        <circle class="path path-1" cx="100" cy="100" r="80"></circle>
+                    </svg>
+
+                    <div class="percentage">70%</div>
+                    <div class="text">Communication</div>
+
+                </div>
+
+                <div class="radial-bars">
+
+                    <svg x="0px" y="0px" viewBox="0 0 200 200">
+                        <circle class="progress-bar" cx="100" cy="100" r="80"></circle>
+                        <circle class="path path-1" cx="100" cy="100" r="80"></circle>
+                    </svg>
+
+                    <div class="percentage">75%</div>
+                    <div class="text">Problem Solving</div>
+
+                </div>
+
+                <div class="radial-bars">
+
+                    <svg x="0px" y="0px" viewBox="0 0 200 200">
+                        <circle class="progress-bar" cx="100" cy="100" r="80"></circle>
+                        <circle class="path path-1" cx="100" cy="100" r="80"></circle>
+                    </svg>
+
+                    <div class="percentage">90%</div>
+                    <div class="text">Team Work</div>
+
+                </div>
+            </div>
+
+        </section>
